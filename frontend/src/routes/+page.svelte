8d6427076084f565d068a553bc73d719e4f17a6c
Sergio Salgado <ssc1099.csmm@gmail.com>
2025-04-13 22:43:25 +0200

    refactor: theme toggle removed

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
   import { page } from '$app/stores';
   import { onMount } from 'svelte';
-  import ThemeToggle from '$lib/components/ThemeToggle.svelte';
   import LayoutContainer from '$lib/components/LayoutContainer.svelte';
   import Hero from '$lib/components/Hero.svelte';
   import SelectedWorks from '$lib/components/SelectedWorks.svelte';
@@ -12,9 +11,8 @@
   export let data;
 </script>
 
-<ThemeToggle />
 
-<div class="bg-neutral-50 dark:bg-neutral-950 text-neutral-900 dark:text-neutral-100 min-h-screen">
+<div class="min-h-screen dark bg-neutral-950 text-neutral-100">
   <LayoutContainer>
     <Hero />
     <SelectedWorks projects={data.projects} />
